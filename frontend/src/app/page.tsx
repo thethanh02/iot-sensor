6e42252aa47d3492ff7c512206e276bab3001dd0
thethanh02 <2002tintin@gmail.com>
2023-09-13 20:06:05 +0700

    fix bug1

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,5 @@
 import { Metadata } from "next"
 
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Overview } from "@/components/overview"
-import LightBulb from "@/components/light-bulb-toggle"
-import Fan from "@/components/fan"
-import { DropletIcon, SunMediumIcon, ThermometerIcon } from "lucide-react"
-import Bulb from "@/components/bulb"
-import { getCurrentSensorVal } from "@/connections/mqtt"
 import HomePage from "@/components/home-page"
 
 export const metadata: Metadata = {
